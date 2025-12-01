ed336cf79537e51ed8645e4b48d963e8a22f02fd
Pheeraphat01 <piyaphonn20549@gmail.com>
2025-12-01 10:54:01 +0700

    Update test.php

--- 92-Maetawinee/test.php
+++ 92-Maetawinee/test.php
@@ -11,7 +11,16 @@
 <link rel="stylesheet" href="style.css">
 
 <body>
-    
+   
+<!-- ส่วนของ Navbar -->
+<nav class="navbar">
+
+<a href="index.php" class="a-navbar">หน้าแรก</a>
+<a href="test.php" class="a-navbar">หน้า test</a>
+
+</nav>
+
+
 <div class="div">
     <h1 class="h1">grace</h1>
 </div>
