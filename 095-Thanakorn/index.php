3367be26b22a7d88f14cac452d1541f5d25001b4
jakjakgapat-byte <15thanakornboonsom@gmail.com>
2025-12-01 11:04:24 +0700

    +

--- 095-Thanakorn/index.php
+++ 095-Thanakorn/index.php
@@ -2,43 +2,45 @@
 <html lang="en">
 
 
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>sixseven</title>
+    
+    <!-- แทรก css -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- ชื่อเว็บไซต์ -->
+    <title>gold digger</title>
 </head>
 
-<link rel="stylesheet" href="style.css">
 <body>
+    
+<!-- NavBar -->
+<nav class="navbar">
 
+<a href="index.php" class="a-navbar">หน้าแรก</a>
+<a href="test.php" class="a-navbar">หน้า test</a>
+
+</nav>
+
+<!-- ส่วนของเนื้อหา -->
+<main class="main">
 
 <div class="div">
     <h1 class="h1">pls speed i need this.</h1>
 </div>
 
+</main>
 
-<h2 class="h2">my mom is kinda homeless</h2>
+<!-- ส่วนของ footer -->
+<footer class="footer">
 
-<img src="./img/1.jpg" alt="" class="">
 
-<p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus quo, esse ratione fugiat ad reiciendis illum nemo iste odit eveniet dolore labore sit delectus eligendi excepturi! Perspiciatis aut repudiandae repellat!
-Quasi repellat mollitia reprehenderit eius vel provident, perspiciatis, enim illo dolores iste ad dignissimos. Mollitia, nam molestiae? Ipsum ipsa ipsam, adipisci cupiditate ad voluptatem quisquam, totam illum quae amet hic!
-Cupiditate quibusdam repudiandae delectus nostrum ut pariatur consequatur. Optio nobis pariatur, atque maiores tempora, est officia laborum perspiciatis illum debitis consectetur voluptates! Est optio consequuntur at adipisci laudantium. Ipsa, vitae.
-Obcaecati officiis optio perferendis culpa quae distinctio suscipit, sequi itaque, aspernatur necessitatibus assumenda facilis at cupiditate nobis deleniti commodi saepe quisquam porro doloremque id consequuntur sit! Esse amet saepe omnis.
-Doloribus accusantium molestiae suscipit! Porro, deleniti. Blanditiis iusto, libero itaque soluta facere voluptatem animi! Cum odio, quae soluta illum quod distinctio id debitis modi odit mollitia quibusdam perspiciatis. Voluptate, architecto?
-Exercitationem fugiat rem esse excepturi ad tempora, beatae error quasi consequatur dignissimos enim quo iure voluptatibus dolorum, odio ex aspernatur porro, veritatis saepe ipsam placeat harum corrupti. Explicabo, mollitia vero!
-Harum labore at cum eligendi? Eum nesciunt temporibus dignissimos autem consectetur dolores qui voluptatum maiores nihil ad consequuntur vel assumenda expedita quis soluta, corporis nam ex! Provident rem sint quos.
-Ipsa iusto aliquam est perspiciatis perferendis asperiores aperiam sed illum, accusantium deleniti repellat facilis laboriosam accusamus tempore odio velit, quisquam, rerum quam nobis! Deserunt incidunt totam voluptas iusto dolor eius?
-Fugiat, dicta minima. Obcaecati neque, vero iusto ullam dolore culpa? Dolorem suscipit magnam accusantium ullam est alias perferendis. Facilis quisquam quibusdam distinctio nesciunt obcaecati non sapiente temporibus placeat, officiis consequatur!
-Pariatur recusandae sed maxime reprehenderit dolorum illo cum minus, nulla dolore explicabo. Praesentium quaerat consectetur delectus at nam unde illum sapiente iusto repudiandae ullam saepe illo, quia voluptatibus culpa natus?
-Autem optio in reiciendis dignissimos! Cum, fuga rerum totam doloribus dolor ipsam dolores aliquid repellendus quidem tempora distinctio dolorum deserunt minus ad excepturi voluptatibus, corrupti temporibus, sint dolorem nihil dignissimos.
-Provident earum, velit cumque culpa ducimus, dolores omnis atque nihil veritatis ab dolorem vitae aperiam at asperiores adipisci molestiae harum doloribus tempora quas, sed voluptatum. Temporibus ratione dolorum autem doloremque.
-Doloremque, asperiores repellat reprehenderit nesciunt laudantium adipisci placeat magnam nihil? Voluptatum tempora eos modi labore alias nisi! Exercitationem labore molestias porro voluptatum quasi qui, earum tenetur quia provident, distinctio dolore?
-Quam placeat dolor magni tenetur nesciunt ad assumenda eius laborum natus, deleniti optio enim est veniam possimus a veritatis facilis praesentium tempore at! Ipsam dicta sint ducimus omnis, expedita vel.
-Voluptate perferendis impedit nihil officia et, repellendus obcaecati totam distinctio deserunt quod saepe similique quis ad nemo architecto? Porro rerum dignissimos consectetur eligendi. Itaque blanditiis fugit officia quaerat perferendis magnam.</p>
+
+</footer>
 
 </body>
+
 
 
 </html>
