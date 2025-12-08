746cae4e2cac53b4b3fc4a81830673956948a836
ganjetawat-design <time.1430ll@gmail.com>
2025-12-08 10:25:28 +0700

    k time wa

---
+++ 090-raksit/map.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<?php include('include/head.php')?>
+
+
+
+<body>
+
+<?php include('include/navbar.php')?>
+
+<!-- ส่วนของเนื้อหา -->
+<main class="main">
+
+<div class="div">
+    <h1 class="h1">Map</h1>
+</div>
+
+<div class="iframe">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d47556.06811951542!2d102.0703351827736!3d14.96287302605846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31194cb6aaaaaaab%3A0xec5f662e93e55bff!2z4LiV4Lil4Liy4LiU4LmA4LiL4Lif4Lin4Lix4LiZ!5e0!3m2!1sth!2sth!4v1765160731291!5m2!1sth!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+</main>
+
+<!-- ส่วนของ footer -->
+<footer class="footer">
+
+
+
+</footer>
+
+</body>
+
+
+
+</html>
--- 090-raksit/youtube.php
+++ 090-raksit/youtube.php
@@ -1,1 +1,73 @@
-    
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<?php include('include/head.php')?>
+
+
+
+<body>
+
+<?php include('include/navbar.php')?>
+
+<!-- ส่วนของเนื้อหา -->
+<main class="main">
+
+<div class="div">
+    <h1 class="h1">Youtube</h1>
+</div>
+
+<div class="iframe">
+<iframe  width="560" height="315" src="https://www.youtube.com/embed/JxjI6Gw8w78?si=klye1TAksIDwiI9a" <!DOCTYPE html>
+<html lang="en">
+
+
+
+<?php include('include/head.php')?>
+
+
+
+<body>
+
+<?php include('include/navbar.php')?>
+
+<!-- ส่วนของเนื้อหา -->
+<main class="main">
+
+<div class="div">
+    <h1 class="h1">Youtube</h1>
+</div>
+
+<div class="iframe">
+<iframe  width="560" height="315" src="https://www.youtube.com/embed/JxjI6Gw8w78?si=klye1TAksIDwiI9a" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+</main>
+
+<!-- ส่วนของ footer -->
+<footer class="footer">
+
+
+
+</footer>
+
+</body>
+
+
+
+</html>
+</div>
+</main>
+
+<!-- ส่วนของ footer -->
+<footer class="footer">
+
+
+
+</footer>
+
+</body>
+
+
+
+</html>
