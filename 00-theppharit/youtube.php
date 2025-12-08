76f0244ee4f03cc284d87a255babe1385229768c
Th.To. <theppharit7@gmail.com>
2025-12-08 09:09:26 +0700

    +

--- 00-theppharit/youtube.php
+++ 00-theppharit/youtube.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- แทรก css -->
-     <link rel="stylesheet" href="style.css">
-
-    <!-- ชื่อเว็บไซต์ -->
-    <title>Hello World</title>
-</head>
+<?php include('include/head.php') ?>
 
 
 
