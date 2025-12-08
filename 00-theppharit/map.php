7d2de128a97393f1d3617c295e6ee6e02d123cf8
Th.To. <theppharit7@gmail.com>
2025-12-08 09:56:44 +0700

    +

--- 00-theppharit/map.php
+++ 00-theppharit/map.php
@@ -4,25 +4,28 @@
 
 <body>
 
-<?php include('include/navbar.php') ?>
+    <?php include('include/navbar.php') ?>
 
-<!-- ส่วนของเนื้อหา -->
-<main class="main">
+    <!-- ส่วนของเนื้อหา -->
+    <main class="main">
 
-<div class="div">
-    <h1 class="h1">map</h1>
-</div>
-
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10651.432206258976!2d102.10461880014194!3d14.980056409531326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31194c99684d59b1%3A0xf187768cb357515d!2z4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4Lit4Liy4LiK4Li14Lin4Lio4Li24LiB4Lip4Liy4LiZ4LiE4Lij4Lij4Liy4LiK4Liq4Li14Lih4Liy!5e1!3m2!1sth!2sth!4v1765160730924!5m2!1sth!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
-</main>
-
-<!-- ส่วนของ footer -->
-<footer class="footer">
+        <div class="div">
+            <h1 class="h1">map</h1>
+        </div>
 
 
+        <div class="iframe">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10651.432206258976!2d102.10461880014194!3d14.980056409531326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31194c99684d59b1%3A0xf187768cb357515d!2z4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4Lit4Liy4LiK4Li14Lin4Lio4Li24LiB4Lip4Liy4LiZ4LiE4Lij4Lij4Liy4LiK4Liq4Li14Lih4Liy!5e1!3m2!1sth!2sth!4v1765160730924!5m2!1sth!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
 
-</footer>
+    </main>
+
+    <!-- ส่วนของ footer -->
+    <footer class="footer">
+
+
+
+    </footer>
 
 </body>
 
