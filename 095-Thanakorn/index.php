ee23e478a0ccb5ea0761121f326ba08d5ce2f5a9
jakjakgapat-byte <jakjakgapat@gmail.com>
2025-12-08 09:10:53 +0700

    fgh

--- 095-Thanakorn/index.php
+++ 095-Thanakorn/index.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!-- แทรก css -->
-    <link rel="stylesheet" href="style.css">
-
-    <!-- ชื่อเว็บไซต์ -->
-    <title>gold digger</title>
-</head>
+<?php include('include/head.php') ?>
 
 <body>
     
