34c275b9bdba52216b7684038d20eeac7f0e1daa
Pheeraphat01 <piyaphonn20549@gmail.com>
2025-12-08 09:28:54 +0700

    ้

--- 92-Maetawinee/map.php
+++ 92-Maetawinee/map.php
@@ -3,15 +3,7 @@
 
 <body>
     
-<!-- ส่วนของ Navbar -->
-<nav class="navbar">
-
-<a href="index.php" class="a-navbar">หน้าแรก</a>
-<a href="test.php" class="a-navbar">หน้า test</a>
-<a href="youtube.php" class="a-navbar">youtube</a>
-<a href="map.php" class="a-navbar">map</a>
-
-</nav>
+<?php include('include/navbar.php') ?>
 
 <!-- ส่วนของเนื้อหา -->
 <main class="main">
@@ -19,6 +11,9 @@
 <div class="div">
     <h1 class="h1">map</h1>
 </div>
+
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3854.271489220154!2d102.08186367587878!3d14.97762716773327!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31194cbadf0a27bb%3A0xd547e6d207ebd350!2z4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4LmA4LiX4LiE4LiZ4Li04LiE4LiZ4LiE4Lij4Lij4Liy4LiK4Liq4Li14Lih4Liy!5e0!3m2!1sth!2sth!4v1765160730772!5m2!1sth!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 </main>
 
