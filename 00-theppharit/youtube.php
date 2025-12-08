9cbe812d46356252d77272a6a78ecfe055c22cf9
Th.To. <theppharit7@gmail.com>
2025-12-08 09:28:55 +0700

    +

--- 00-theppharit/youtube.php
+++ 00-theppharit/youtube.php
@@ -13,6 +13,8 @@
     <h1 class="h1">Youtube</h1>
 </div>
 
+<iframe width="560" height="315" src="https://www.youtube.com/embed/6J373vtjn6Y?si=WKBsQ2lsJz8i-VVu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
 </main>
 
 <!-- ส่วนของ footer -->
