4c206830737d5ae3fbd00a73fbf05af486c4010f
Pheeraphat01 <piyaphonn20549@gmail.com>
2025-12-08 10:20:51 +0700

    j

--- 92-Maetawinee/map.php
+++ 92-Maetawinee/map.php
@@ -12,8 +12,9 @@
     <h1 class="h1">map</h1>
 </div>
 
-
+<div class="iframe">
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3854.271489220154!2d102.08186367587878!3d14.97762716773327!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31194cbadf0a27bb%3A0xd547e6d207ebd350!2z4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4LmA4LiX4LiE4LiZ4Li04LiE4LiZ4LiE4Lij4Lij4Liy4LiK4Liq4Li14Lih4Liy!5e0!3m2!1sth!2sth!4v1765160730772!5m2!1sth!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
 
 </main>
 
