ccba40f169a2f2538646374ff3e4d04d6c37dc3c
Th.To. <theppharit7@gmail.com>
2025-12-08 09:18:48 +0700

    +

--- 00-theppharit/index.php
+++ 00-theppharit/index.php
@@ -4,13 +4,7 @@
 
 <body>
 
-<!-- ส่วนของ Navbar -->
-<nav class="navbar">
-
-<a href="index.php" class="a-navbar">หน้าแรก</a>
-<a href="test.php" class="a-navbar">หน้า test</a>
-<a href="youtube.php" class="a-navbar">youtube</a>
-<a href="map.php" class="a-navbar">map</a>
+<?php include('include/navbar.php') ?>
 
 </nav>
 
