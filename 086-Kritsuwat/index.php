acb9084bddcdc909746cf5f37ffeacc30fd563fe
ganjetawat-design <time.1430ll@gmail.com>
2025-12-08 09:09:25 +0700

    d

--- 086-Kritsuwat/index.php
+++ 086-Kritsuwat/index.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!-- แทรกcss -->
-     <link rel="stylesheet" href="style.css">
-
-    <!-- ชื้อเว็บไซต์ -->
-    <title>HaHaHa</title>
-</head>
+<?php include('include/head.php')?>
 
 
 
