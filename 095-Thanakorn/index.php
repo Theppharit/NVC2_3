42c802fad16504ad525e1b90de468b6f10b7d858
jakjakgapat-byte <jakjakgapat@gmail.com>
2025-12-08 09:20:28 +0700

    ghj

--- 095-Thanakorn/index.php
+++ 095-Thanakorn/index.php
@@ -2,15 +2,7 @@
 
 <body>
     
-<!-- NavBar -->
-<nav class="navbar">
-
-<a href="index.php" class="a-navbar">หน้าแรก</a>
-<a href="test.php" class="a-navbar">หน้า test</a>
-<a href="youtube.php" class="a-navbar">youtube</a>
-<a href="map.php" class="a-navbar">mapt</a>
-
-</nav>
+<?php include('include/navbar.php') ?>
 
 <!-- ส่วนของเนื้อหา -->
 <main class="main">
