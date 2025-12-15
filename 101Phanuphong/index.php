903d9c2edb953a8b6e12c364c5b61f7821118cf2
ganjetawat-design <time.1430ll@gmail.com>
2025-12-15 10:25:04 +0700

    h

--- 101Phanuphong/index.php
+++ 101Phanuphong/index.php
@@ -1,35 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<link rel="stylesheet" href="style.css">
+<?php include('include/head.php')?>
+
+
+
 <body>
+
+<?php include('include/navbar.php')?>
+
+<!-- ส่วนของเนื้อหา -->
+<main class="main">
+
 <div class="div">
-<h1 class="">sawad</h1>
+    <h1 class="h1">k</h1>
 </div>
-<h2 class="">wawa</h2>
 
-<img src="./img/1.jpg" alt="" class="">
 
-<p class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis quas ipsa sapiente rem deleniti illum. Molestias modi perspiciatis aperiam illo voluptas reprehenderit dolorum voluptates provident sint earum adipisci, nesciunt ducimus.
-Qui repudiandae, voluptas nobis corrupti, esse omnis harum quasi porro consequuntur excepturi eligendi. Nemo harum cupiditate consequatur sit, itaque deleniti delectus quibusdam in, doloribus modi quo veniam ipsum praesentium quia?
-Neque suscipit aperiam quae maxime velit, nostrum labore dolor voluptatibus iste dolorum, asperiores error totam ab incidunt at quibusdam voluptas eos aut tenetur ipsum vero cupiditate animi? Quisquam, nulla nam.
-Voluptatem nam libero dolores corrupti, odio et provident voluptatum dignissimos harum veniam at iste ex incidunt quidem ipsum magnam, reiciendis ipsa nesciunt animi suscipit fugit! Illo autem ad laborum quaerat.
-Iusto cumque vel quas? Architecto quo illum, rem optio quaerat harum cupiditate repudiandae nam voluptatibus quibusdam aut magni consectetur cumque veniam blanditiis eius eveniet quisquam minus! Ipsum quasi porro rem.
-Exercitationem, hic incidunt? Perferendis id possimus officiis praesentium dolorum dolore, vero numquam quam obcaecati voluptatum vel hic odio, quia iure, ducimus nostrum aspernatur deserunt. Deserunt ratione adipisci sapiente ipsum asperiores?
-Quae, nisi facere, adipisci vitae quibusdam omnis libero aperiam illum ut quo ipsam! Culpa recusandae itaque, pariatur consectetur deleniti a nisi numquam sint quis at non ullam veniam blanditiis eveniet.
-Iste, reprehenderit consectetur impedit nostrum dolores eveniet accusamus! Officia a, adipisci sequi modi magni qui odio! Molestiae ducimus inventore impedit sint vel similique excepturi velit iste, harum pariatur. Unde, illum!
-Quis adipisci soluta itaque sit doloremque cupiditate sunt iusto tempora necessitatibus perferendis veniam amet nisi, voluptatem placeat, aspernatur omnis cumque voluptate nam. Minus inventore asperiores libero quibusdam. Eligendi, aut cumque?
-Tempore esse illum repellat amet. Ipsum totam culpa veritatis ut quas officiis. Nisi velit expedita, exercitationem est accusantium delectus voluptatum. Cupiditate tempora odit totam vero nam temporibus accusantium aut nobis.
-Obcaecati dignissimos amet placeat asperiores autem doloremque, fuga, iusto impedit molestiae, eos nobis nulla sed sit aliquid sequi ea consequuntur. Commodi, distinctio. Harum facilis quo architecto, aliquid totam soluta quos!
-Tempore ex pariatur deleniti nam accusantium sunt fugit, quod, neque deserunt ducimus amet! Atque doloribus illo vitae, dolor excepturi similique corrupti quisquam dolorem, earum facere, accusamus eum rem vel ab?
-Odio incidunt dolores sapiente mollitia modi minus ipsum reprehenderit perferendis culpa repudiandae, quos vel nemo dolorum, amet eligendi eveniet eum suscipit ut fugiat ducimus voluptatem harum facere animi ipsa! Natus?
-Cumque doloribus quisquam, perferendis ad corrupti officiis odio veniam perspiciatis est deserunt consequatur, numquam ut maiores eius porro explicabo quibusdam, aliquam non expedita quasi! Dolorem non doloremque fugiat dolorum laborum.
-Aliquid eum doloremque neque saepe fugiat a labore. Ipsam cupiditate deserunt quos odio voluptates dicta ab quasi facilis harum, praesentium dignissimos dolorem culpa, temporibus ipsa voluptatem voluptatum. Veniam, debitis pariatur.</p>
+</main>
 
+<!-- ส่วนของ footer -->
+<footer class="footer">
+
+
+
+</footer>
 
 </body>
 
