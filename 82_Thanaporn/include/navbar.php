3f8df2f4e4fb4fc793cd4235f396ba455e5f9601
mahathepsin <studyno1mahathep@gmail.com>
2025-12-15 11:02:40 +0700

    .

--- 82_Thanaporn/include/navbar.php
+++ 82_Thanaporn/include/navbar.php
@@ -1,11 +1,17 @@
 <!-- ส่วนของ Navbar -->
 <nav class="navbar">
 
-<a href="index.php" class="a-navber">หน้้าแรก</a>
+<div class="navber">
+    <a href="index.php"class="a-navber">หน้าแรก</a>
+
+</div>
+
+<div class="div-navder">
 <a href="test.php" class="a-navber">หน้า test</a>
 <a href="youtube.php" class="a-navber">หน้า youtube</a>
 <a href="map.php" class="a-navber">หน้า map</a>
 
-
+</div>
+</Nav>
 
 
