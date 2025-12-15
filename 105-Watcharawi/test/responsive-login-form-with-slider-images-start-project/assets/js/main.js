37c8600cc3f7df7e17af46d6ae44ab532cf3f0fb
VakerikPhattharin <bossgg2551@gmail.com>
2025-12-15 10:45:31 +0700

    .

--- 105-Watcharawi/test/responsive-login-form-with-slider-images-start-project/assets/js/main.js
+++ 105-Watcharawi/test/responsive-login-form-with-slider-images-start-project/assets/js/main.js
@@ -15,3 +15,20 @@ showHiddenPass('loginPass','loginEye')
 
 
 /*=============== SWIPER IMAGES ===============*/
+const swiperLogin = new Swiper('.login__swiper', {
+   loop: true,
+   spaceBetween: '24',
+   grabCursor: true,
+   speed: 600,
+   // effect: 'fade',
+
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+
+   autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+   },
+})
