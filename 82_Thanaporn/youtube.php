6fe37676a07edb3f527da02d75a10c1b78899717
mahathepsin <studyno1mahathep@gmail.com>
2025-12-15 10:30:08 +0700

    .

---
+++ 82_Thanaporn/youtube.php
@@ -0,0 +1,33 @@
+<?php include('include/head.php') ?>
+
+
+
+<body>
+
+    <?php include('include/navbar.php') ?>
+
+    <!-- ส่วนของเนื้อหา -->
+    <main class="main">
+
+        <div class="div">
+            <h1 class="h1">Youtube</h1>
+        </div>
+
+        <div class="iframe">
+            <iframe width="560" height="315" src="<iframe width="560" height="315" src="https://www.youtube.com/embed/wUosOYWzFlQ?si=EscZjFs8-INPXYM8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+
+    </main>
+
+    <!-- ส่วนของ footer -->
+    <footer class="footer">
+
+
+
+    </footer>
+
+</body>
+
+
+
+</html>
