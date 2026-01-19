e47c99fed84ad3e025056cbf4c9e1c5eead3a136
Th.To. <theppharit7@gmail.com>
2026-01-19 09:52:21 +0700

    +

--- 00-theppharit/include/footer.php
+++ 00-theppharit/include/footer.php
@@ -1,6 +1,12 @@
 <!-- ส่วนของ footer -->
 <footer class="footer">
 
+<div class="div-footer">
+    <div class="colum-footer"></div>
+    <div class="colum-footer"></div>
+    <div class="colum-footer"></div>
+</div>
 
+<div class="div-footer"></div>
 
 </footer>
