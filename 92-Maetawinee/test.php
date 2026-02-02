33497894198bc0957653c66a993e846eb1ee8c4e
xp <grace.sirirak01@gmail.com>
2026-02-02 11:04:57 +0700

    Update test.php

--- 92-Maetawinee/test.php
+++ 92-Maetawinee/test.php
@@ -11,13 +11,16 @@
     <h1 class="h1">grace</h1>
 </div>
 
-<main class="test"></main>
+<main class="test">
+
 
 <h2 class="h2">DekInw</h2>
 
 <img src="./img/1.jpg" alt="" class="img-test">
 
 <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis neque ducimus saepe sequi, fuga tenetur mollitia nesciunt expedita eum voluptatibus enim debitis esse nam ut cumque beatae distinctio vel odio.</p>
+
+</main>
 
 </html>
 
